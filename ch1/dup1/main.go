42c35d79325832992018ee89bf970af87d58925c
하재권 <jaekwon.ha@sk.com>
2020-12-03 23:38:59 +0900

    연습문제 1.3: 방식에 상관없이 최초 실행시에 느리게 측정되어 여러번 반복시켰다

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
-func main() {
-	for i, args := range os.Args {
-		fmt.Printf("%d %s\n", i, args)
+func inefficient() {
+	fmt.Println("inefficient")
+	start := time.Now()
+	s := os.Args[1]
+	for _, args := range os.Args[2:] {
+		s = s + " " + args
 	}
+	fmt.Println(s)
+	mill := time.Since(start).Nanoseconds()
+	fmt.Printf("elapsed time: %d\n", mill)
+}
+
+func efficient() {
+	fmt.Println("efficient")
+	start := time.Now()
+	s := strings.Join(os.Args[1:], " ")
+	fmt.Println(s)
+	mill := time.Since(start).Nanoseconds()
+	fmt.Printf("elapsed time: %d\n", mill)
+}
+
+func main() {
+	efficient()
+	inefficient()
+	efficient()
+	inefficient()
+	efficient()
+	inefficient()
 }
