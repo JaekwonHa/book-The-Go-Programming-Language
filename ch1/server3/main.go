7ef64eb6f849ee7a2dabae16d26741138ef2f880
JaekwonHa <hazxz1116@gmail.com>
2020-12-04 12:23:27 +0900

    ch 1.7 lissajous

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/lissajous", handlerLissajous)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handlerLissajous(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +35,42 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
+}
+
+var palette = []color.Color{color.White, color.Black, color.RGBA{G: 255, A: 255}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+	greenIndex = 2
+)
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+
+			x = math.Sin(t + 0.1)
+			y = math.Sin((t+0.1)*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: 인코딩 오류 무시
 }
