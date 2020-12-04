cf7c6d161c4d9d8acac21fa571945b03bd61fcf9
JaekwonHa <hazxz1116@gmail.com>
2020-12-04 12:06:27 +0900

    연습문제 1.10: 두번쩨 fetchall 시에 2배 가량 빠름

--- ch1/fetchall/main.go
+++ ch1/fetchall/main.go
@@ -6,19 +6,25 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 	ch := make(chan string)
+	var result string
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch)
 	}
 	for range os.Args[1:] {
-		fmt.Println(<-ch)
+		result += <-ch
 	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	result += fmt.Sprintf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf(result)
+
+	uuid, _ := exec.Command("uuidgen").Output()
+	ioutil.WriteFile("./fetchall_result_"+string(uuid), []byte(result), 0644)
 }
 
 func fetch(url string, ch chan<- string) {
@@ -31,9 +37,9 @@ func fetch(url string, ch chan<- string) {
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
 		return
 	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs %7d %s\n", secs, nbytes, url)
 }
