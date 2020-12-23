95ab36c23aa559d6f1e88f3b7f2236b330648057
JaekwonHa <hazxz1116@gmail.com>
2020-12-23 22:10:05 +0900

    ch 4.4 treesort

---
+++ ch4/treesort/treesort_test.go
@@ -0,0 +1,20 @@
+package treesort
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	data := make([]int, 50)
+	for i := range data {
+		data[i] = rand.Int() % 50
+	}
+	Sort(data)
+	if !sort.IntsAreSorted(data) {
+		t.Errorf("not sorted: %v", data)
+	}
+	fmt.Printf("data: %v", data)
+}
